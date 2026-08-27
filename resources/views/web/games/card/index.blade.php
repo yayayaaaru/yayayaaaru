0a7df2471b1d7e9c1c9ad1c7c6a8d48bc5bcd5d7
Noilty <thenoilty@gmail.com>
2026-08-27 16:04:55 +0300

    fixed *

--- resources/views/web/games/card/index.blade.php
+++ resources/views/web/games/card/index.blade.php
@@ -110,7 +110,7 @@
                         </div>
                     </div>
                     <div class="mt-auto">
-                        <div class="d-flex" title="Удалена {{ $game->removed_at->ago() }}">
+                        <div class="d-flex">
                             <a
                                 href="#"
                                 @class([
@@ -124,16 +124,16 @@
                             >
                                 @if(is_null($game->removed_at))
                                     <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    class="icon icon-tabler icons-tabler-filled icon-tabler-player-play m-0"
-                                >
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                    <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"/>
-                                </svg>
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                        class="icon icon-tabler icons-tabler-filled icon-tabler-player-play m-0"
+                                    >
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"/>
+                                    </svg>
                                 @else
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
