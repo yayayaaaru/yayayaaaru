1c1b92c6f6316a0eed9e2fd7140f8d3cdfd4863c
Noilty <thenoilty@gmail.com>
2026-08-30 16:03:43 +0300

    fixed *

--- database/migrations/2026_08_19_131534_change_released_at_and_description_to_games_table.php
+++ database/migrations/2026_08_19_131534_change_released_at_and_description_to_games_table.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,6 +17,20 @@ return new class extends Migration
         Schema::table('games', function (Blueprint $table) {
             $table->dateTime('released_at')->nullable()->change()->after('views_count');
             $table->longText('description')->nullable()->change()->after('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('games')->whereNull('released_at')->update(['released_at' => now()]);
+        DB::table('games')->whereNull('description')->update(['description' => '']);
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->dateTime('released_at')->nullable(false)->change();
+            $table->longText('description')->nullable(false)->change();
         });
     }
 };
